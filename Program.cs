84da6fccd4aedb1963333c3799b8304d92184755
Ekaterina <tebekartina@ya.ru>
2022-08-18 10:25:55 +0400

    есть код для нахождения большего из трех чисел

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿{
+    int a, b, c;
+   
+        Console.WriteLine("Введите три числа");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+
+        c = Convert.ToInt32(Console.ReadLine());
+        int max = 0;
+            
+ 
+           { if (a >= b == a >= c)
+                max = a;
+            else if (b >= a == b >=c )
+                max = b;
+            else if (c >= a == c >= b)
+                max = c;
+           }
+     Console.WriteLine ("Максимальное число {1}, max");
+     Console.ReadLine();
+}
+
+
+
+  
